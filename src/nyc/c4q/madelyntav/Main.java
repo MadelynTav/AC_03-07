71851dbbb862247453e045af060cdbab2463b673
MadelynTav <Madelyntav@gmail.com>
2015-03-07 16:19:32 -0500

    Adding the calculator

---
+++ src/nyc/c4q/madelyntav/Main.java
@@ -0,0 +1,52 @@
+package nyc.c4q.madelyntav;
+//Madelyn Tavarez And Charlyn Buchanan(Charlyn23)
+//Polish Calculator
+//C4Q NYC
+//Working with variables and math functions
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner input= new Scanner(System.in);
+        int number;
+        int numberTwo;
+
+        char operator;
+
+        System.out.println("Please enter Your First Number");
+        number=input.nextInt();
+        System.out.println("Please enter the second number");
+        numberTwo=input.nextInt();
+        System.out.println("Please enter an operator");
+        operator= input.next().charAt(0);
+
+
+        if (operator=='+'){
+            System.out.println("Result is "+ number+numberTwo);
+        }
+
+        else if (operator=='-'){
+            System.out.println("Result is "+(number-numberTwo));
+        }
+        else if (operator=='*'){
+            System.out.println("Result is "+number*numberTwo);
+
+        }
+        else if (operator=='%'){
+            System.out.println("Result is "+number%numberTwo);
+        }
+        else if (operator=='/'){
+            System.out.println("Result is "+number/numberTwo);
+        }
+        else {
+            System.out.println("Not A Valid Operator!");
+        }
+
+
+
+
+
+    }
+}
